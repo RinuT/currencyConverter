a9b5c525187b1c9392996c237d34d322a307e67f
RinuT <40629768+RinuT@users.noreply.github.com>
2019-03-09 23:43:43 -0500

    Add files via upload

---
+++ backend/src/main/java/com/SpringApp/CurrencyConverter/controller/CurrencyCalculatorController.java
@@ -0,0 +1,35 @@
+package com.SpringApp.CurrencyConverter.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.SpringApp.CurrencyConverter.model.Currency;
+import com.SpringApp.CurrencyConverter.util.CurrencyConverter;
+
+/**
+ * 
+ * @author Rinu
+ * Accepts request body from post call ending with value currency is mapped to currency object
+ * returns response with calculated new value amount
+ */
+@CrossOrigin("*")
+@RestController
+public class CurrencyCalculatorController {
+	
+	@Autowired
+	private CurrencyConverter currencyConverter;
+	
+	@RequestMapping(method=RequestMethod.POST,value="currency")
+	public ResponseEntity<?> calculate(@RequestBody Currency currency) {
+	
+		currency.setAmount(currencyConverter.convert(currency.getAmount()));
+		return ResponseEntity.ok().body(currency);
+
+	}
+
+}
